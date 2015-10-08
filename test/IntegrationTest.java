86b8290cef6470ea74be20775b4b9a7499fcfb17
Ben McCann <ben@connectifier.com>
2015-10-07 19:28:32 -0700

    A database is not needed to trigger the bug

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.assertTrue;
 import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
 
@@ -14,7 +13,7 @@ public class IntegrationTest {
 
   @Test
   public void test() {
-    running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+    running(testServer(3333, fakeApplication()), HTMLUNIT, new Callback<TestBrowser>() {
       public void invoke(TestBrowser browser) {
         browser.goTo("http://localhost:3333");
         assertTrue(browser.pageSource().contains("Your new application is ready."));
